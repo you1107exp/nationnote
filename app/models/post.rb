49cac9bc85ad98ba1240c6d32c941b82a9f491c1
you1107exp <royal.code0028@gmail.com>
2020-09-07 17:47:47 +0900

    アソシエーションとバリデーションの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :country
+  belongs_to :user
+
+  validates :title, :country, :content, presence: true
+  validates :country_id, numericality: { other_than: 1 }
 end
