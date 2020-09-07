5ed42298c4cba797e32ea890e29e9141eb12fa10
you1107exp <royal.code0028@gmail.com>
2020-09-07 18:26:41 +0900

    createアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    post = Post.create(post_params)
+    if post.save
+      redirect_to root_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :country_id, :content, :ref_url, :ref_img, :nickname)
+  end
+
 end
