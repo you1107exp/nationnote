de6fc4cb5f55f5ae915094a3ecc757b557c4c7d5
you1107exp <royal.code0028@gmail.com>
2020-09-08 22:14:16 +0900

    updateアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,17 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    if post.valid?
+      post.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def post_params
