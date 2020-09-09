c771cded98bc1e94cd37c745a9161620f39abcb0
you1107exp <royal.code0028@gmail.com>
2020-09-09 22:18:04 +0900

    投稿からユーザー情報も先に読み込む(N+1解消)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.includes(:user).order("created_at DESC")
   end
   
   def new
