6bf411506bf58dc6fe7b634d400219b236b01a33
you1107exp <royal.code0028@gmail.com>
2020-09-10 15:16:43 +0900

    要素の並べ替え、ページ間リンクの設定

--- app/controllers/country_posts_controller.rb
+++ app/controllers/country_posts_controller.rb
@@ -2,7 +2,7 @@ class CountryPostsController < ApplicationController
 
   def show
     @country = Country.find(params[:id])
-    @posts = Post.where(country_id: params[:id])
+    @posts = Post.where(country_id: params[:id]).order("created_at DESC")
   end
 
 end
