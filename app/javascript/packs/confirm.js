ffc438323df34ac46909bbf7bf273793485acd19
you1107exp <royal.code0028@gmail.com>
2020-09-13 20:15:02 +0900

    削除ボタンクリックでconfirmダイアログを表示

---
+++ app/javascript/packs/confirm.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('delete-btn').addEventListener('click', function(){
+    window.confirm("削除してよろしいですか？");
+  });
+});
